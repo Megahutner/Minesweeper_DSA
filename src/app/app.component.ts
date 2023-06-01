e4eb446ee2d6d5fedd0ff9f2edab744fbc78be2e
Megahunter <53591352+Megahutner@users.noreply.github.com>
2023-06-02 02:58:40 +0700

    Finish basic game logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,32 +5,56 @@ import { Component, OnInit, Inject, ElementRef, ViewChild, DoCheck } from '@angu
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit, DoCheck{
   @ViewChild('myCanvas') canvasRef!: ElementRef 
 
 
-
+ 
+  // enums
   public Dificulty={
     Easy : 1,
     Normal : 2,
     Hard : 3,
   }
 
+  public BlockType = {
+    Empty: 0 ,
+    Mine : 1 ,
+  }
+  // general variables
+  MineBlocks: any = [];
   width: any;
   height: any;
   shapedimension: any;
   shapes: any;
-  dificultyPopup: boolean = true;
   oldDificulty: any;
   currentDificulty: any;
   flagCount: any;
   mine: any;
+  color: any;
+  empty: any;
 
-  
+
+  // popup flags
+  dificultyPopup: boolean = true;
+  gameoverPopup : boolean = false;
+  winPopup: boolean = false;
+
   
 
   ngOnInit(): void {
-      
+    enum Color {
+      "#7FFF00",
+      "#0000FF",
+      "#008B8B",
+      "#8B008B",
+      "#006400",
+      "#9932CC",
+      "#FF8C00",
+      "#A52A2A",
+    }
+    this.color =  Color;
   }
   ngDoCheck(): void {
     if(this.width != null && this.height != null && this.oldDificulty != this.currentDificulty ){
@@ -48,20 +72,23 @@ export class AppComponent implements OnInit, DoCheck{
       this.shapedimension = 70;
       this.flagCount = 10;
       this.mine = 10;
+      this.empty = 90;
     }
-    else if(data == this.Dificulty.Normal){  // 20 x 16 board
-      this.width = 1000;
+    else if(data == this.Dificulty.Normal){  // 16 x 16 board
+      this.width = 800;
       this.height = 800;
       this.shapedimension = 50;
       this.flagCount = 20;
       this.mine = 50;
+      this.empty = 270;
     }
-    else if(data == this.Dificulty.Hard){ // 30 x 24 board
-      this.width = 1200;
-      this.height = 640;
+    else if(data == this.Dificulty.Hard){ // 24 x 24 board
+      this.width = 960;
+      this.height = 960;
       this.shapedimension = 40;
       this.flagCount = 50;
       this.mine = 99;
+      this.empty = 477;
     }
     this.currentDificulty = data;
     this.dificultyPopup = false;
@@ -84,11 +111,7 @@ export class AppComponent implements OnInit, DoCheck{
         let y = j * this.shapedimension;
         let isOpened = false;
         let isFlagged = false;
-        // let F = 100000;
-        // let G = 100000;
-        // let H = 100000;
         let neighbors = new Array();
-        //ctx.strokeRect(x, y, this.shapedimension, this.shapedimension); // draw grid
         ctx.fillStyle = "#696969";
         ctx.fillRect(x, y, this.shapedimension, this.shapedimension);
 
@@ -97,8 +120,6 @@ export class AppComponent implements OnInit, DoCheck{
         ctx.strokeRect(x, y, this.shapedimension, this.shapedimension);
         this.shapes[i][j] = { x, y,i,j, type, neighbors, isOpened , isFlagged};
       }
-     
-
     }
     let mineCount = 0;
     while(mineCount < this.mine){
@@ -111,8 +132,11 @@ export class AppComponent implements OnInit, DoCheck{
        else{
         this.shapes[tempI][tempJ].type = 1;
         mineCount++;
+        this.MineBlocks.push(this.shapes[tempI][tempJ]);
        }
     }
+    console.log(this.MineBlocks);
+    //this.shapes[0][0].type = 1;
   }
 
 
@@ -174,31 +198,117 @@ export class AppComponent implements OnInit, DoCheck{
             console.log(this.shapes[i][j])
             const ctx: CanvasRenderingContext2D = this.canvasRef.nativeElement.getContext('2d');
             if(!this.shapes[i][j].isFlagged){
-              this.shapes[i][j].isFlagged = true;
-              let flag = new Image();
-              flag.src = "assets/mine.png";
-              flag.onload = () => {
-                ctx.clearRect(this.shapes[i][j].x, this.shapes[i][j].y, this.shapedimension,  this.shapedimension );
-                ctx.strokeStyle = "#0000ff";
-                ctx.lineWidth   = 2;
-                ctx.strokeRect(this.shapes[i][j].x+1, this.shapes[i][j].y+1, this.shapedimension-2,  this.shapedimension-2 );
-                ctx.drawImage(flag,this.shapes[i][j].x, this.shapes[i][j].y, this.shapedimension,  this.shapedimension )
-                
+              if(this.shapes[i][j].type === 1) // if block has mine
+              {
+                this.gameOver();
+              } 
+              else{
+                console.log("open")
+                this.openNeighborBlocks(i,j);
+                if(this.empty === 0){
+                  this.winPopup = true;
+                }
+              } 
               }
-            }
-            else{
-             
-            }
-           
+            }           
           }
       }
     }
-    
 
+    openNeighborBlocks(i: number, j: number){
+      if(!this.shapes[i][j].isOpened && !this.shapes.isFlagged){
+      const ctx: CanvasRenderingContext2D = this.canvasRef.nativeElement.getContext('2d');
+      if(this.shapes[i][j].type === 0 ){
+        var neighborMineCount = 0;        
+        for(let x = i-1; x <= i+1; x++) {
+          for(let y = j-1; y<= j+1;y++){
+            if((x >= 0 && x < this.shapes.length) && (y >=0 && y< this.shapes[0].length)){
+              console.log(x,y)
+            if(this.shapes[x][y].type === 1){
+              neighborMineCount++;
+            }
+          }
+          }
+        }
+        console.log(neighborMineCount)
+        if(neighborMineCount === 0){
+          ctx.clearRect(this.shapes[i][j].x, this.shapes[i][j].y, this.shapedimension,  this.shapedimension );
+          ctx.fillStyle = "#F0F8FF";
+          ctx.fillRect(this.shapes[i][j].x, this.shapes[i][j].y, this.shapedimension, this.shapedimension);
+          ctx.strokeStyle = "#0000ff";
+          ctx.lineWidth   = 2;
+          ctx.strokeRect(this.shapes[i][j].x, this.shapes[i][j].y, this.shapedimension, this.shapedimension);
+          this.shapes[i][j].isOpened = true;
+          this.empty --;
+          if(i - 1 >= 0){
+            this.openNeighborBlocks(i - 1, j);
+          }
+          if(j - 1 >= 0){
+            this.openNeighborBlocks(i, j -1);
+          }
+          if(i + 1 < this.shapes.length){
+            this.openNeighborBlocks(i + 1, j);
+          }
+          if(j + 1 < this.shapes[0].length){
+            this.openNeighborBlocks(i, j + 1);
+          }
+        }
+        else{
+          ctx.clearRect(this.shapes[i][j].x, this.shapes[i][j].y, this.shapedimension,  this.shapedimension );
+          ctx.fillStyle = this.color[neighborMineCount-1];
+          ctx.textAlign = "center";
+          ctx.font = "30px Arial"
+          ctx.fillText(`${neighborMineCount}`,this.shapes[i][j].x + this.shapedimension/2,this.shapes[i][j].y + this.shapedimension/1.5);
+          ctx.strokeStyle = "#0000ff";
+          ctx.lineWidth   = 2;
+          ctx.strokeRect(this.shapes[i][j].x, this.shapes[i][j].y, this.shapedimension, this.shapedimension);
+          this.shapes[i][j].isOpened = true;
+          this.empty --;
+        }
+      }
+    }
+    }
+    gameOver(){
+      const ctx: CanvasRenderingContext2D = this.canvasRef.nativeElement.getContext('2d');
+      for(let i = 0; i< this.shapes.length; i++){
+        for(let j = 0; j< this.shapes[0].length; j++){
+          if(this.shapes[i][j].type === 1){ 
+          this.shapes[i][j].isOpened = true;  
+          let flag = new Image();
+          flag.src = "assets/mine.png";
+          flag.onload = () => {
+          ctx.clearRect(this.shapes[i][j].x, this.shapes[i][j].y, this.shapedimension,  this.shapedimension );
+          ctx.strokeStyle = "#0000ff";
+          ctx.lineWidth   = 2;
+          ctx.strokeRect(this.shapes[i][j].x+1, this.shapes[i][j].y+1, this.shapedimension-2,  this.shapedimension-2 );
+          ctx.drawImage(flag,this.shapes[i][j].x, this.shapes[i][j].y, this.shapedimension,  this.shapedimension )
+        }
+      }
+      
+    }
+  }
+      // this.MineBlocks.array.forEach((i: any) => { // draw mine blocks on the board
+      // }
+      // );
+      // show game over popup
+      this.gameoverPopup = true;
 
+    }
 
+    retry(){
+      this.gameoverPopup = false;
+      this.dificultyPopup = false;
+      this.gameoverPopup = false;
+      this.selectDificulty(this.currentDificulty); // reset variables
+      this.drawBoard();
+    }
 
+    changeDifficulty(){
+      this.gameoverPopup = false;
+      this.dificultyPopup = true;
+      this.winPopup = false;
+    }
   }
 
 
-}
+
